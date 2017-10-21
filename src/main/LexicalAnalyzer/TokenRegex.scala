08e71939df6a85dec6d6c133fba92cbe7974d594
mohammaduzair9 <31093445+mohammaduzair9@users.noreply.github.com>
2017-10-22 04:47:15 +0500

    Updated Token Regular Expressions

--- src/main/LexicalAnalyzer/TokenRegex.scala
+++ src/main/LexicalAnalyzer/TokenRegex.scala
@@ -4,29 +4,29 @@ import scala.util.matching.Regex
 
 object TokenRegex {
 
-  val DATA_TYPE = "^(int|bool|alpha)$".r
-  val INTEGER = "-?\\d+".r
-  val BOOLEAN = "^(tt|ff)$".r
-  val ALPHA = "^([\"|\']).*([\"|\'])$".r
-  val IDENTIFIER = "[a-zA-Z][A-Za-z0-9_$*#]*".r
-  val VAR_TYPE = "(var)".r
-  val CONST_TYPE = "(const)".r
+  val DATA_TYPE = "int|bool|alpha".r
+  val INTEGER = "\\d+".r
+  val BOOLEAN = "tt|ff".r
+  val ALPHA = """(["'])[^"']+\1""".r
+  val IDENTIFIER = "[a-zA-Z][a-zA-Z0-9$_*#]*".r
+  val VAR_TYPE = "var".r
+  val CONST_TYPE = "const".r
   val IF = "(if)".r
-  val THEN = "(then)".r
-  val ELSE = "(else)".r
-  val DO = "(do)".r
-  val WHILE = "(while)".r
-  val COLON = "[:]{1}".r
+  val THEN = "then".r
+  val ELSE = "else".r
+  val DO = "do".r
+  val WHILE = "while".r
+  val COLON = ":".r
   val ASSIGNMENT = "[=]{1}".r
   val SPACE = " ".r
-  val BREAK = """\n|;""".r
-  val NIL = "(nil)".r
+  val BREAK = ";|\\n".r
+  val NIL = "nil".r
   val UOP = "-|not".r
   val BOP = "==|><|and|or|\\^|>|<".r
   val PLUS = "\\+".r
   val MUL = "\\*".r
   val DIV = "\\/".r
-  val PRINT = "(print)".r
-  val SKIP = "^(skip)$".r
+  val PRINT = "print".r
+  val SKIP = "skip".r
 
 }
--- src/main/main.scala
+++ src/main/main.scala
@@ -6,7 +6,7 @@ object main {
   def main(args: Array[String]) {
     val var_table = Map[String,identifier]().withDefaultValue(new identifier("null","null",0))
 
-    val str:String = "var x:int = 3;if x>10 then print x+10 else print x"
+    val str:String = "var x:alpha = \"hh\" ;if x>10 then print x+10 else print x"
     val tokenizer: Lexer = new Lexer(str)
     val parser: Parser = new Parser(tokenizer)
     val interpreter = new Evaluator(parser, var_table)
